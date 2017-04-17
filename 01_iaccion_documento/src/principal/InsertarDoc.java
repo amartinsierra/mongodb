e3f60fbeb9c4a65400483c6b84a45ea1d1eb3691
admin <admin@gmail.com>
2017-04-17 19:17:25 +0200

    commit 9

--- 01_iaccion_documento/src/principal/InsertarDoc.java
+++ 01_iaccion_documento/src/principal/InsertarDoc.java
@@ -15,6 +15,7 @@ import com.mongodb.client.MongoDatabase;
 //commit 6666666
 //commit 77777
 //Commit 8888888
+//commit 99999999
 public class InsertarDoc {
 
 	public static void main(String[] args) {
