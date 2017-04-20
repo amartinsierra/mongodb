0ab61c3f3a2cfb0cdbe5868f81e384be067e78fa
antonio <antonio@gmail.com>
2017-04-20 14:49:25 +0200

    commit 12

--- 01_iaccion_documento/src/principal/InsertarDoc.java
+++ 01_iaccion_documento/src/principal/InsertarDoc.java
@@ -9,9 +9,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-
-//commit 4444
-//commit once
+// commit2 restaurado
 public class InsertarDoc {
 
 	public static void main(String[] args) {
